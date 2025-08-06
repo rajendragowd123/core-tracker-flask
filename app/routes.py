602430b24f3941f7fff44ebdfd59ddf2892f8c67
Rajendra Gowd Burra <rajendragowd575@gmail.com>
2025-08-06 21:56:25 +0530

    Day 4: Added base.html and templae inheritance

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
